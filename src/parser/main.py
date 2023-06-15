0122160d6c5b7cd686558eda91224a53f0e2678d
Joshua Jones <joshua.ar.jones@gmail.com>
2023-06-15 01:29:16 +0100

    [feat] allow comparison between 2 documents

--- src/parser/main.py
+++ src/parser/main.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 from typing import List
+
 from docx import Document
 from openpyxl import Workbook
 
@@ -21,15 +22,85 @@ class Header:
     text: str
     children: List["Header"] | None = None
 
-    def add_child(self, child):
+    def add_child(self, child: "Header"):
         if self.children is None:
             self.children = []
         self.children.append(child)
 
+    def compare(self, other: "Header") -> List[str]:
+        missing_items = []
 
-if __name__ == "__main__":
-    input_file = Path("tests", "fixtures", "parser_test.docx")
-    project_document = WordDocument.make(input_file)
+        # Check if the current header is missing in other instance
+        if self.level != other.level or self.text != other.text:
+            missing_items.append(self.text)
+
+        # Recursively check for missing children in other instance
+        if self.children is not None:
+            for child in self.children:
+                matching_child = next(
+                    (
+                        c
+                        for c in other.children or []
+                        if c.level == child.level and c.text == child.text
+                    ),
+                    None,
+                )
+                if matching_child is None:
+                    missing_items.append(child.text)
+                else:
+                    missing_items.extend(child.compare(matching_child))
+
+        return missing_items
+
+
+def build_header_hierarchy(headers: List[Header]) -> Header:
+    """_summary_
+
+    Args:
+        headers (List[Header]): _description_
+
+    Returns:
+        Header: _description_
+    """
+    root_header = Header(0, "ROOT")
+    level_1_header = None
+    level_2_header = None
+    for header in reversed(headers):
+        if header.level == 1:
+            root_header.add_child(header)
+            level_1_header = header
+            level_2_header = None
+        elif header.level == 2:
+            level_1_header.add_child(header)
+            level_2_header = header
+        elif header.level == 3:
+            if level_2_header is None:
+                level_2_header = Header(2, "")
+                level_1_header.add_child(level_2_header)
+            level_2_header.add_child(header)
+    return root_header
+
+
+def flatten_root(root_header: Header) -> list[str]:
+    headers_list = []
+    for child in root_header.children:
+        print(child.text)
+        headers_list.append((child.level, child.text))
+        if child.children:
+            for childchild in child.children:
+                print(childchild.text)
+                headers_list.append((childchild.level, childchild.text))
+                if childchild.children:
+                    for childchildchild in childchild.children:
+                        print(childchildchild.text)
+                        headers_list.append(
+                            (childchildchild.level, childchildchild.text)
+                        )
+    return headers_list
+
+
+def load_headers_from_file(file: Path) -> List[Header]:
+    project_document = WordDocument.make(file)
     word_document = Document(project_document.path)
     content = word_document.paragraphs
     headers = [para for para in content if para.style.name.startswith("Heading")]
@@ -46,48 +117,48 @@ if __name__ == "__main__":
             case _:
                 level = 0
         list_of_headers.append(Header(level=level, text=header.text))
+    return list_of_headers
 
-    def build_header_hierarchy(headers: List[Header]) -> Header:
-        root_header = Header(0, "ROOT")
-        level_1_header = None
-        level_2_header = None
-        for header in reversed(headers):
-            if header.level == 1:
-                root_header.add_child(header)
-                level_1_header = header
-                level_2_header = None
-            elif header.level == 2:
-                level_1_header.add_child(header)
-                level_2_header = header
-            elif header.level == 3:
-                if level_2_header is None:
-                    level_2_header = Header(2, "")
-                    level_1_header.add_child(level_2_header)
-                level_2_header.add_child(header)
-        return root_header
 
-    root_header = build_header_hierarchy(list_of_headers)
-
-    def flatten_root(root_header: Header) -> list[str]:
-        headers_list = []
-        for child in root_header.children:
-            print(child.text)
-            headers_list.append((child.level, child.text))
-            if child.children:
-                for childchild in child.children:
-                    print(childchild.text)
-                    headers_list.append((childchild.level, childchild.text))
-                    if childchild.children:
-                        for childchildchild in childchild.children:
-                            print(childchildchild.text)
-                            headers_list.append(
-                                (childchildchild.level, childchildchild.text)
-                            )
-        return headers_list
-
+def write_to_excel_file(root_header: Header):
     wb = Workbook()
     ws = wb.active
     ws.append(["Level", "Heading"])
     for head in flatten_root(root_header=root_header):
         ws.append([head[0], head[1]])
     wb.save("output.xlsx")
+
+
+if __name__ == "__main__":
+    input_file = Path("tests", "fixtures", "parser_test.docx")
+    list_of_headers = load_headers_from_file(input_file)
+    root_header = build_header_hierarchy(list_of_headers)
+
+    check_file = Path("tests", "fixtures", "parser_test_no_match.docx")
+    list_of_headers_to_check = load_headers_from_file(check_file)
+    check_header = build_header_hierarchy(list_of_headers_to_check)
+
+    write_to_excel_file(root_header)
+
+    workbook = Workbook()
+    sheet = workbook.active
+
+    max_col = sheet.max_column
+    sheet.cell(row=1, column=1, value="Level")
+    sheet.cell(row=1, column=2, value="Heading")
+
+    for row, value in enumerate(flatten_root(root_header=root_header), start=2):
+        sheet.cell(row=row, column=max_col, value=value[0])
+        sheet.cell(row=row, column=max_col + 1, value=value[1])
+
+    max_col = sheet.max_column + 2
+    sheet.cell(row=1, column=4, value="Level")
+    sheet.cell(row=1, column=5, value="Heading")
+    for row, value in enumerate(flatten_root(root_header=check_header), start=2):
+        sheet.cell(row=row, column=max_col, value=value[0])
+        sheet.cell(row=row, column=max_col + 1, value=value[1])
+
+    sheet.cell(row=sheet.max_row + 2, column=1, value="Missing columns")
+    sheet.append(root_header.compare(check_header))
+
+    workbook.save("output2.xlsx")
--- src/parser/tests/test_main.py
+++ src/parser/tests/test_main.py
@@ -1,4 +1,6 @@
-from ..main import Header
+from pathlib import Path
+
+from ..main import Header, build_header_hierarchy, load_headers_from_file
 
 
 def test_add_child():
@@ -21,3 +23,39 @@ def test_add_child():
     header4.add_child(header3)
     assert len(header4.children) == 1
     assert header4.children[0] == header3
+
+
+def test_can_compare_documents() -> None:
+    list_of_headers_1 = load_headers_from_file(
+        Path("tests", "fixtures", "parser_test.docx")
+    )
+    list_of_headers_2 = load_headers_from_file(
+        Path("tests", "fixtures", "parser_test_match.docx")
+    )
+    root_header_1 = build_header_hierarchy(list_of_headers_1)
+    root_header_2 = build_header_hierarchy(list_of_headers_2)
+    assert root_header_1 == root_header_2
+
+
+def test_can_detect_mismatch() -> None:
+    list_of_headers_1 = load_headers_from_file(
+        Path("tests", "fixtures", "parser_test.docx")
+    )
+    list_of_headers_2 = load_headers_from_file(
+        Path("tests", "fixtures", "parser_test_no_match.docx")
+    )
+    root_header_1 = build_header_hierarchy(list_of_headers_1)
+    root_header_2 = build_header_hierarchy(list_of_headers_2)
+    assert root_header_1 != root_header_2
+
+
+def test_missing_headers() -> None:
+    input_file = Path("tests", "fixtures", "parser_test.docx")
+    list_of_headers = load_headers_from_file(input_file)
+    root_header = build_header_hierarchy(list_of_headers)
+    check_file = Path("tests", "fixtures", "parser_test_no_match.docx")
+    list_of_headers_to_check = load_headers_from_file(check_file)
+    check_header = build_header_hierarchy(list_of_headers_to_check)
+    assert root_header.compare(check_header) == [
+        "Heading 2 subheading 2 subsubheading 1"
+    ]
